2fbb9cee05bb9b68fa328e91d22ce8edffefcd47
RomanRobotnik <rnavarro@robotnik.es>
2021-05-04 15:48:00 +0200

    Params to add offset in angular vel and linear acc

--- src/myahrs_driver.cpp
+++ src/myahrs_driver.cpp
@@ -37,6 +37,8 @@
 #include <sensor_msgs/MagneticField.h>
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_datatypes.h>
+#include <geometry_msgs/Vector3Stamped.h>
+#include <geometry_msgs/Vector3.h>
 
 //------------------------------------------------------------------------------
 using namespace WithRobot;
@@ -52,6 +54,7 @@ private:
   ros::Publisher imu_data_pub_;
   ros::Publisher imu_mag_pub_;
   ros::Publisher imu_temperature_pub_;
+  ros::Publisher imu_rpy_pub_;
 
   tf::TransformBroadcaster broadcaster_;
 
@@ -64,6 +67,8 @@ private:
   double angular_velocity_stddev_;
   double magnetic_field_stddev_;
   double orientation_stddev_;
+  geometry_msgs::Vector3 linear_acceleration_offset_;
+  geometry_msgs::Vector3 angular_velocity_offset_;
 
   void OnSensorData(int sensor_id, SensorData data)
   {
@@ -78,9 +83,7 @@ private:
   }
 
 public:
-  MyAhrsDriverForROS(std::string port="", int baud_rate=115200)
-  : iMyAhrsPlus(port, baud_rate),
-    nh_priv_("~")
+  MyAhrsDriverForROS(std::string port = "", int baud_rate = 115200) : iMyAhrsPlus(port, baud_rate), nh_priv_("~")
   {
     // dependent on user device
     nh_priv_.setParam("port", port);
@@ -95,15 +98,25 @@ public:
     nh_priv_.param("angular_velocity_stddev", angular_velocity_stddev_, 0.002428);
     nh_priv_.param("magnetic_field_stddev", magnetic_field_stddev_, 0.00000327486);
     nh_priv_.param("orientation_stddev", orientation_stddev_, 0.002143);
+
+    nh_priv_.param("linear_acceleration_offset_x", linear_acceleration_offset_.x, 0.0);
+    nh_priv_.param("linear_acceleration_offset_y", linear_acceleration_offset_.y, 0.0);
+    nh_priv_.param("linear_acceleration_offset_z", linear_acceleration_offset_.z, 0.0);
+    nh_priv_.param("angular_velocity_offset_x", angular_velocity_offset_.x, 0.0);
+    nh_priv_.param("angular_velocity_offset_y", angular_velocity_offset_.y, 0.0);
+    nh_priv_.param("angular_velocity_offset_z", angular_velocity_offset_.z, 0.0);
+
     // publisher for streaming
-    imu_data_raw_pub_   = nh_.advertise<sensor_msgs::Imu>("imu/data_raw", 1);
-    imu_data_pub_       = nh_.advertise<sensor_msgs::Imu>("imu/data", 1);
-    imu_mag_pub_        = nh_.advertise<sensor_msgs::MagneticField>("imu/mag", 1);
-    imu_temperature_pub_= nh_.advertise<std_msgs::Float64>("imu/temperature", 1);
+    imu_data_raw_pub_ = nh_.advertise<sensor_msgs::Imu>("imu/data_raw", 1);
+    imu_data_pub_ = nh_.advertise<sensor_msgs::Imu>("imu/data", 1);
+    imu_mag_pub_ = nh_.advertise<sensor_msgs::MagneticField>("imu/mag", 1);
+    imu_temperature_pub_ = nh_.advertise<std_msgs::Float64>("imu/temperature", 1);
+    imu_rpy_pub_ = nh_.advertise<geometry_msgs::Vector3Stamped>("imu/rpy", 1);
   }
 
   ~MyAhrsDriverForROS()
-  {}
+  {
+  }
 
   bool initialize()
   {
@@ -111,16 +124,20 @@ public:
 
     do
     {
-      if(start() == false) break;
-      //Euler angle(x, y, z axis)
-      //IMU(linear_acceleration, angular_velocity, magnetic_field)
-      if(cmd_binary_data_format("EULER, IMU") == false) break;
+      if (start() == false)
+        break;
+      // Euler angle(x, y, z axis)
+      // IMU(linear_acceleration, angular_velocity, magnetic_field)
+      if (cmd_binary_data_format("EULER, IMU") == false)
+        break;
       // 100Hz
-      if(cmd_divider("1") == false) break;
+      if (cmd_divider("1") == false)
+        break;
       // Binary and Continue mode
-      if(cmd_mode("BC") == false) break;
+      if (cmd_mode("BC") == false)
+        break;
       ok = true;
-    } while(0);
+    } while (0);
 
     return ok;
   }
@@ -143,62 +160,101 @@ public:
     sensor_msgs::Imu imu_data_msg;
     sensor_msgs::MagneticField imu_magnetic_msg;
     std_msgs::Float64 imu_temperature_msg;
+    geometry_msgs::Vector3Stamped imu_rpy_msg;
 
     double linear_acceleration_cov = linear_acceleration_stddev_ * linear_acceleration_stddev_;
-    double angular_velocity_cov    = angular_velocity_stddev_ * angular_velocity_stddev_;
-    double magnetic_field_cov      = magnetic_field_stddev_ * magnetic_field_stddev_;
-    double orientation_cov         = orientation_stddev_ * orientation_stddev_;
+    double angular_velocity_cov = angular_velocity_stddev_ * angular_velocity_stddev_;
+    double magnetic_field_cov = magnetic_field_stddev_ * magnetic_field_stddev_;
+    double orientation_cov = orientation_stddev_ * orientation_stddev_;
 
-    imu_data_raw_msg.linear_acceleration_covariance[0] =
-    imu_data_raw_msg.linear_acceleration_covariance[4] =
-    imu_data_raw_msg.linear_acceleration_covariance[8] =
-    imu_data_msg.linear_acceleration_covariance[0] =
-    imu_data_msg.linear_acceleration_covariance[4] =
-    imu_data_msg.linear_acceleration_covariance[8] = linear_acceleration_cov;
+    imu_data_raw_msg.linear_acceleration_covariance[0] = imu_data_raw_msg.linear_acceleration_covariance[4] =
+        imu_data_raw_msg.linear_acceleration_covariance[8] = imu_data_msg.linear_acceleration_covariance[0] =
+            imu_data_msg.linear_acceleration_covariance[4] = imu_data_msg.linear_acceleration_covariance[8] =
+                linear_acceleration_cov;
 
-    imu_data_raw_msg.angular_velocity_covariance[0] =
-    imu_data_raw_msg.angular_velocity_covariance[4] =
-    imu_data_raw_msg.angular_velocity_covariance[8] =
-    imu_data_msg.angular_velocity_covariance[0] =
-    imu_data_msg.angular_velocity_covariance[4] =
-    imu_data_msg.angular_velocity_covariance[8] = angular_velocity_cov;
+    imu_data_raw_msg.angular_velocity_covariance[0] = imu_data_raw_msg.angular_velocity_covariance[4] =
+        imu_data_raw_msg.angular_velocity_covariance[8] = imu_data_msg.angular_velocity_covariance[0] =
+            imu_data_msg.angular_velocity_covariance[4] = imu_data_msg.angular_velocity_covariance[8] =
+                angular_velocity_cov;
 
-    imu_data_msg.orientation_covariance[0] =
-    imu_data_msg.orientation_covariance[4] =
-    imu_data_msg.orientation_covariance[8] = orientation_cov;
+    imu_data_msg.orientation_covariance[0] = imu_data_msg.orientation_covariance[4] =
+        imu_data_msg.orientation_covariance[8] = orientation_cov;
 
-    imu_magnetic_msg.magnetic_field_covariance[0] =
-    imu_magnetic_msg.magnetic_field_covariance[4] =
-    imu_magnetic_msg.magnetic_field_covariance[8] = magnetic_field_cov;
+    imu_magnetic_msg.magnetic_field_covariance[0] = imu_magnetic_msg.magnetic_field_covariance[4] =
+        imu_magnetic_msg.magnetic_field_covariance[8] = magnetic_field_cov;
 
-    static double convertor_g2a  = 9.80665;    // for linear_acceleration (g to m/s^2)
-    static double convertor_d2r  = M_PI/180.0; // for angular_velocity (degree to radian)
-    static double convertor_r2d  = 180.0/M_PI; // for easy understanding (radian to degree)
-    static double convertor_ut2t = 1000000;    // for magnetic_field (uT to Tesla)
-    static double convertor_c    = 1.0;        // for temperature (celsius)
+    static double convertor_g2a = 9.80665;       // for linear_acceleration (g to m/s^2)
+    static double convertor_d2r = M_PI / 180.0;  // for angular_velocity (degree to radian)
+    static double convertor_r2d = 180.0 / M_PI;  // for easy understanding (radian to degree)
+    static double convertor_ut2t = 1000000;      // for magnetic_field (uT to Tesla)
+    static double convertor_c = 1.0;             // for temperature (celsius)
 
     double roll, pitch, yaw;
 
     // original sensor data used the degree unit, convert to radian (see ROS REP103)
     // we used the ROS's axes orientation like x forward, y left and z up
     // so changed the y and z aixs of myAHRS+ board
-    roll  =  sensor_data_.euler_angle.roll*convertor_d2r;
-    pitch = -sensor_data_.euler_angle.pitch*convertor_d2r;
-    yaw   = -sensor_data_.euler_angle.yaw*convertor_d2r;
+    roll = sensor_data_.euler_angle.roll * convertor_d2r;
+    pitch = -sensor_data_.euler_angle.pitch * convertor_d2r;
+    yaw = -sensor_data_.euler_angle.yaw * convertor_d2r;
 
-    ImuData<float>& imu = sensor_data_.imu;
+    if (sensor_data_.imu_type == RAW)
+    {
+      ImuData<int>& imu = sensor_data_.imu_rawdata;
+      // original data used the g unit, convert to m/s^2
+      imu_data_raw_msg.linear_acceleration.x = imu_data_msg.linear_acceleration.x =
+          imu.ax * convertor_g2a + linear_acceleration_offset_.x;
+      imu_data_raw_msg.linear_acceleration.y = imu_data_msg.linear_acceleration.y =
+          -imu.ay * convertor_g2a + linear_acceleration_offset_.y;
+      imu_data_raw_msg.linear_acceleration.z = imu_data_msg.linear_acceleration.z =
+          -imu.az * convertor_g2a + linear_acceleration_offset_.z;
+
+      // original data used the degree/s unit, convert to radian/s
+      imu_data_raw_msg.angular_velocity.x = imu_data_msg.angular_velocity.x =
+          imu.gx * convertor_d2r + angular_velocity_offset_.x;
+      imu_data_raw_msg.angular_velocity.y = imu_data_msg.angular_velocity.y =
+          -imu.gy * convertor_d2r + angular_velocity_offset_.y;
+      imu_data_raw_msg.angular_velocity.z = imu_data_msg.angular_velocity.z =
+          -imu.gz * convertor_d2r + angular_velocity_offset_.z;
+
+      // original data used the celsius unit
+      imu_temperature_msg.data = imu.temperature;
+    }
+    else
+    {
+      ImuData<float>& imu = sensor_data_.imu;
+      // original data used the g unit, convert to m/s^2
+      imu_data_raw_msg.linear_acceleration.x = imu_data_msg.linear_acceleration.x =
+          imu.ax * convertor_g2a + linear_acceleration_offset_.x;
+      imu_data_raw_msg.linear_acceleration.y = imu_data_msg.linear_acceleration.y =
+          -imu.ay * convertor_g2a + linear_acceleration_offset_.y;
+      imu_data_raw_msg.linear_acceleration.z = imu_data_msg.linear_acceleration.z =
+          -imu.az * convertor_g2a + linear_acceleration_offset_.z;
+
+      // original data used the degree/s unit, convert to radian/s
+      imu_data_raw_msg.angular_velocity.x = imu_data_msg.angular_velocity.x =
+          imu.gx * convertor_d2r + angular_velocity_offset_.x;
+      imu_data_raw_msg.angular_velocity.y = imu_data_msg.angular_velocity.y =
+          -imu.gy * convertor_d2r + angular_velocity_offset_.y;
+      imu_data_raw_msg.angular_velocity.z = imu_data_msg.angular_velocity.z =
+          -imu.gz * convertor_d2r + angular_velocity_offset_.z;
+
+      // original data used the uTesla unit, convert to Tesla
+      imu_magnetic_msg.magnetic_field.x = imu.mx / convertor_ut2t;
+      imu_magnetic_msg.magnetic_field.y = -imu.my / convertor_ut2t;
+      imu_magnetic_msg.magnetic_field.z = -imu.mz / convertor_ut2t;
+
+      // original data used the celsius unit
+      imu_temperature_msg.data = imu.temperature;
+    }
 
     tf::Quaternion orientation = tf::createQuaternionFromRPY(roll, pitch, yaw);
 
     ros::Time now = ros::Time::now();
 
-    imu_data_raw_msg.header.stamp =
-    imu_data_msg.header.stamp     =
-    imu_magnetic_msg.header.stamp = now;
+    imu_data_raw_msg.header.stamp = imu_data_msg.header.stamp = imu_magnetic_msg.header.stamp = now;
 
-    imu_data_raw_msg.header.frame_id =
-    imu_data_msg.header.frame_id     =
-    imu_magnetic_msg.header.frame_id = frame_id_;
+    imu_data_raw_msg.header.frame_id = imu_data_msg.header.frame_id = imu_magnetic_msg.header.frame_id = frame_id_;
 
     // orientation
     imu_data_msg.orientation.x = orientation[0];
@@ -206,48 +262,29 @@ public:
     imu_data_msg.orientation.z = orientation[2];
     imu_data_msg.orientation.w = orientation[3];
 
-    // original data used the g unit, convert to m/s^2
-    imu_data_raw_msg.linear_acceleration.x =
-    imu_data_msg.linear_acceleration.x     =  imu.ax * convertor_g2a;
-    imu_data_raw_msg.linear_acceleration.y =
-    imu_data_msg.linear_acceleration.y     = -imu.ay * convertor_g2a;
-    imu_data_raw_msg.linear_acceleration.z =
-    imu_data_msg.linear_acceleration.z     = -imu.az * convertor_g2a;
-
-    // original data used the degree/s unit, convert to radian/s
-    imu_data_raw_msg.angular_velocity.x =
-    imu_data_msg.angular_velocity.x     =  imu.gx * convertor_d2r;
-    imu_data_raw_msg.angular_velocity.y =
-    imu_data_msg.angular_velocity.y     = -imu.gy * convertor_d2r;
-    imu_data_raw_msg.angular_velocity.z =
-    imu_data_msg.angular_velocity.z     = -imu.gz * convertor_d2r;
-
-    // original data used the uTesla unit, convert to Tesla
-    imu_magnetic_msg.magnetic_field.x =  imu.mx / convertor_ut2t;
-    imu_magnetic_msg.magnetic_field.y = -imu.my / convertor_ut2t;
-    imu_magnetic_msg.magnetic_field.z = -imu.mz / convertor_ut2t;
-
-    // original data used the celsius unit
-    imu_temperature_msg.data = imu.temperature;
+    // RPY
+    imu_rpy_msg.header.stamp = now;
+    imu_rpy_msg.header.frame_id = frame_id_;
+    imu_rpy_msg.vector.x = roll;
+    imu_rpy_msg.vector.y = pitch;
+    imu_rpy_msg.vector.z = yaw;
 
     // publish the IMU data
     imu_data_raw_pub_.publish(imu_data_raw_msg);
     imu_data_pub_.publish(imu_data_msg);
     imu_mag_pub_.publish(imu_magnetic_msg);
     imu_temperature_pub_.publish(imu_temperature_msg);
+    imu_rpy_pub_.publish(imu_rpy_msg);
 
     // publish tf
-    if(publish_tf)
+    if (publish_tf)
     {
-      broadcaster_.sendTransform(tf::StampedTransform(tf::Transform(tf::createQuaternionFromRPY(roll, pitch, yaw),
-                                 tf::Vector3(0.0, 0.0, 0.0)),
-                                 ros::Time::now(),
-                                 frame_id_,
-                                 parent_frame_id_));
+      broadcaster_.sendTransform(
+          tf::StampedTransform(tf::Transform(tf::createQuaternionFromRPY(roll, pitch, yaw), tf::Vector3(0.0, 0.0, 0.0)),
+                               ros::Time::now(), frame_id_, parent_frame_id_));
     }
   }
 };
-
 
 //------------------------------------------------------------------------------
 int main(int argc, char* argv[])
@@ -255,14 +292,14 @@ int main(int argc, char* argv[])
   ros::init(argc, argv, "myahrs_driver");
 
   std::string port = std::string("/dev/ttyACM0");
-  int baud_rate    = 115200;
+  int baud_rate = 115200;
 
   ros::param::get("~port", port);
   ros::param::get("~baud_rate", baud_rate);
 
   MyAhrsDriverForROS sensor(port, baud_rate);
 
-  if(sensor.initialize() == false)
+  if (sensor.initialize() == false)
   {
     ROS_ERROR("%s\n", "Initialize() returns false, please check your devices.");
     return 0;
